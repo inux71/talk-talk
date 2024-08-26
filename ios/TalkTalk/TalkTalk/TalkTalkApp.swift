09637983c6f1f6e7b27d66552166f9173491550b
Kacper Grabiec <grabiec.kacper01@gmail.com>
2024-08-26 22:48:06 +0200

    [ios] add project files

---
+++ ios/TalkTalk/TalkTalk/TalkTalkApp.swift
@@ -0,0 +1,17 @@
+//
+//  TalkTalkApp.swift
+//  TalkTalk
+//
+//  Created by Kacper Grabiec on 26/08/2024.
+//
+
+import SwiftUI
+
+@main
+struct TalkTalkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
